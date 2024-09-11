e271021b420b3af3f50fffa7cc4da0a606c51267
Luiz Gonzaga <luizg.academico@gmail.com>
2024-09-11 16:13:01 -0300

    Alerta caso pedido não encontrado

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -3,10 +3,13 @@ class ClientController < ApplicationController
     if params[:codigo_rastreio].present?
       conn = Faraday.new(url: 'http://localhost:3000')
       response = conn.get("rastreio/#{params[:codigo_rastreio]}")
-      @order = JSON.parse(response.body)
-      puts @order
-    else
-      flash[:alert] = "Pedido não encontrado."
+      puts response.body
+      if response.body === ''
+        flash[:alert] = "Pedido não encontrado."
+      else
+        @order = JSON.parse(response.body)
+        flash[:alert] = "Pedido encontrado."
+      end
     end
   end
 
